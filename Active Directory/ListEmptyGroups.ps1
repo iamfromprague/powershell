8fe8b479a896694d6b925ec807c34f429792a93f
iamfromprague <119420141+iamfromprague@users.noreply.github.com>
2022-12-21 13:17:53 +0100

    Update ListEmptyGroups.ps1

--- Active Directory/ListEmptyGroups.ps1
+++ Active Directory/ListEmptyGroups.ps1
@@ -1,12 +1,12 @@
 Import-Module ActiveDirectory
 
 # Get a list of all security groups in the domain
-$groups = Get-ADGroup -Filter {GroupCategory -eq 'Security'} -Properties Members
+$groups = Get-ADGroup -Filter {GroupCategory -eq 'Security'} -Properties Members, DistinguishedName
 
 # Iterate through each group and check if it has any members
 foreach ($group in $groups) {
-  # If the group has no members, output the group name
+  # If the group has no members, output the group name and distinguished name
   if ($group.Members.Count -eq 0) {
-    Write-Output $group.Name
+    Write-Output "$($group.Name) - $($group.DistinguishedName)"
   }
 }
